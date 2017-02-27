cd3842a26587c4c615a8a62dece59a1f4d7e13ef
ciec20151104696 <s20151104696@mac15.local>
2017-02-27 11:50:09 +0800

    Initial Commit

---
+++ text swift 0227/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  text swift 0227
+//
+//  Created by s20151104696 on 17/2/27.
+//  Copyright © 2017年 s20151104696. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
